3cf24131dcccc67b63271b69e76fba5c28dc88a9
somename <someemail>
2016-10-21 14:58:21 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -10,6 +10,8 @@ public class TestPlanetExplorer {
 	static String NO_OBSTACLES = "";
 	static String NO_COMMAND = "";
 	static String LANDING_STATUS = "(0,0,N)";
+	static String TURN_LEFT_INPLACE = "l";
+	static String TURN_RIGHT_INPLACE = "l";
 	PlanetExplorer explorer = new PlanetExplorer(DEFAULT_WIDTH, DEFAULT_HEIGHT, NO_OBSTACLES);
 	
 	
