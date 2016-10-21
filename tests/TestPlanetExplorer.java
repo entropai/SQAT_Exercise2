06632d1f173d6d9ce750f0f33e378281195a913e
somename <someemail>
2016-10-21 14:48:01 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -5,18 +5,30 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	
+	static int DEFAULT_WIDTH  = 100;
+	static int DEFAULT_HEIGHT = 100;
+	static String NO_OBSTACLES = "";
+	
+	PlanetExplorer explorer = new PlanetExplorer(DEFAULT_WIDTH, DEFAULT_HEIGHT, NO_OBSTACLES);
+	
+	
 	@Test
-	public void test_createNewPlanetWithoutObstacles() {
-		
-		int width = 100;
-		int height = 100;
-		PlanetExplorer explorer = new PlanetExplorer(width, height, "");
-		int x = explorer.getPlanetWidth();
-		int y = explorer.getPlanetHeight();
-		assertEquals("Created planet width is in correct ", width, x);
-		assertEquals("Created planet height is in correct ", height, x);
-		
+	public void test_createNewPlanetWithoutObstacles() {		
+		/*PlanetExplorer explorer = new PlanetExplorer(width, height "");
+		int map[][] = explorer.getMap();
+		assertNotEquals(null, map);*/
 	}
+	
+	@Test
+	public void test_createNewPlanetSizeMatchesWithoutObstacles() {		
+		
+		PlanetExplorer explorer = new PlanetExplorer(DEFAULT_WIDTH, DEFAULT_HEIGHT, "");
+		int x = explorer.getPlanetWidht();
+		int y = explorer.getPlanetHeight();
+		assertEquals("Created planet width is in correct ", DEFAULT_WIDTH, x);
+		assertEquals("Created planet height is in correct ", DEFAULT_HEIGHT, y);		
+	}
+	
 	
 	@Test
 	public void test_executeCommand() {
